2f7e7700d5b6fa882745cb2bdf78ee1cf6463d98
Chris Thompson <cthompson2@dcpmidstream.com>
2017-04-04 13:54:28 -0600

    [MAJOR] Provide significantly more control over processing and loading
    * filter - Used to determine which directories and files to include
    * fileTransform - Use as a custom loader or to ignore a file
    * dirTransform - Can prevent a dir from being added to the result

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
 dir-obj
 =======
 
-Create an object from a directory tree.  Create a key with an object for each directory by name.  All files within a directory that can be required (`.js` or `.json`) are added to the directory object, using the base filename as the key and `require('file.js')` as the value.
+Create an object from a directory tree.
+
+`dir-obj` creates an object with keys for each directory (by name).
+All files within a directory that can be required (`.js` or `.json`) are added to the directory object, using the base filename as the key and `require('file.js')` as the value.
 
 This is useful for things like loading test fixture data.
 
+## API
+You can also customize how `dir-obj` processes the directory tree
+
+```javascript
+const fixtures = dirObj.readDirectory(path.join(__dirname, 'fixtures'), {
+  // Use the `filter` option to filter all directories and files
+  // Return `true` to include the directory or file (call `file.isDirectory` to check)
+  // Return `false` to exclude the file or directory from the result
+  // The default is to only allow `.js` and `.json` files
+  filter: file => (file.isDirectory || file.isRequirable) && file.name !== 'index.js',
+  // This function takes a File object and returns the value to be added to the object
+  // You can mutate `File.key` to change the object key used to reference the value
+  // You can return `undefined` to not include the file in the result
+  // The default `fileTransform` just calls `require(file.fullpath)`
+  // Example: Change whitespace in a filename to `_` then set the value using `require`
+  fileTransform: file => {
+    file.key = file.basename.replace(/\s/g, '_');
+    return require(file.fullpath);
+  },
+  // This function takes a File (which is a directory) and a value, which is the recursively
+  // calculated value of that directory
+  // Example: Do not include any directories that have no child keys
+  dirTransform: (file, value) => Object.getOwnPropertyNames(value).length > 0 ? value : undefined
+});
+
 ## Example
+Using the default options:
 
 ```javascript
 /*
@@ -30,4 +59,16 @@ describe('test', function () {
       .expect(200, fixtures.ok.response.success);
   });
 });
+```
+
+Loading SQL files:
+
+```javascript
+// Get an object where the value of each key is a string loaded from each SQL file
+
+const fixtures = dirObj.readDirectory(path.resolve('../../models/sql'), {
+  filter: file => file.isDirectory || file.ext === '.sql'.
+  fileTransform: file => file.readSync() // helper function equivilent to `fs.readFileSync(file.fullpath, 'utf8')`
+});
+
 ```
--- test/index.js
+++ test/index.js
@@ -1,32 +1,234 @@
 'use strict';
 
-const assert = require('assert');
 const dirObj = require('..');
 
-const expected = { 
-  fixtures:
-   { '3dir': { fixtures3: { name: 'dir2/fixtures3.sjon' } },
-     '4.dir': { '_-_bad_%_file__name_': { name: ' - bad % file \\ name .json' } },
-     'dir.2':
-      { subdir1:
-         { subsubdir1:
-            { subfixture1: { name: 'subsubdir1/subfixture1.json' },
-              subfixture2: true },
-           subsubdir2: { index: { name: 'subsubdir2/index.js' } } } },
-     dir1:
-      { fixtures1: { name: 'fixture1.json' },
-        fixtures2: { name: 'fixtures2.js' } },
-     emptydir: {},
-     root: 'This is the root' },
-  index: {} 
+const assert = require('assert');
+const path = require('path');
+const fixturePath = path.join(__dirname, 'fixtures');
+
+const test = (name, fn) => {
+  try {
+    fn();
+    return true;
+  } catch (e) {
+    console.log('Failed test: ' + name);
+    console.log(e.stack);
+    return false;
+  }
 };
 
-const result = dirObj.readDirectory(__dirname, { 
-  fileTransform: (value, file) => {
-    file.key = file.basename.replace(/\s/g, '_');
-    return value;
-  }
-});
+test('dir-obj', () => {
+  const tests = [
 
-assert.deepEqual(result, expected);
-console.log('test passed');
+    test('no arguments requires all js and json files', () => {
+      const expected = {
+        fixtures1: {
+          'name': 'fixture1.json'
+        },
+        fixtures2: {
+          name: 'fixtures2.js'
+        }
+      };
+      const result = dirObj.readDirectory(path.join(__dirname, '/fixtures/dir1'));
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('filter - only load JSON files', () => {
+      const expected = {
+        subdir1: {
+          subsubdir1: {
+            subfixture1: {
+              'name': 'subsubdir1/subfixture1.json'
+            }
+          },
+          subsubdir2: {}
+        }
+      };
+
+      const result = dirObj.readDirectory(path.join(fixturePath, 'dir.2'), {
+        filter: file => file.isDirectory || file.ext === '.json'
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('filter - do not add keys for empty directories', () => {
+      const expected = {
+        'dir.2': {
+          subdir1: {
+            subsubdir1: { subfixture2: true },
+            subsubdir2: {
+              index: { name: 'subsubdir2/index.js' }
+            }
+          }
+        },
+        dir1: {
+          fixtures2: { name: 'fixtures2.js' }
+        },
+        root: 'This is the root'
+      };
+
+      const result = dirObj.readDirectory(fixturePath, {
+        filter: file => file.isDirectory || file.ext === '.js',
+        dirTransform: (file, value) => (Object.getOwnPropertyNames(value).length > 0) ? value : undefined
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('fileTransform - change the object key', () => {
+      const expected = {
+        '3dir': { FIXTURES3: { name: 'dir2/fixtures3.sjon' } },
+        '4.dir': { '_-_BAD_%_FILE__NAME_': { name: ' - bad % file \\ name .json' } },
+        'dir.2': {
+          subdir1: {
+            subsubdir1: {
+              SUBFIXTURE1: { name: 'subsubdir1/subfixture1.json' },
+              SUBFIXTURE2: true
+            },
+            subsubdir2: { INDEX: { name: 'subsubdir2/index.js' } }
+          }
+        },
+        dir1: {
+          FIXTURES1: { name: 'fixture1.json' },
+          FIXTURES2: { name: 'fixtures2.js' }
+        },
+        ROOT: 'This is the root'
+      };
+
+      const result = dirObj.readDirectory(fixturePath, {
+        fileTransform: file => {
+          file.key = file.key.replace(/[. ]/g, '_').toUpperCase();
+          return require(file.fullpath);
+        }
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('fileTransform - use a custom file loader to get data from non-requireable files', () => {
+      const expected = {
+        dir1: { fixtures2: 'This will not get loaded\n' }
+      };
+
+      const result = dirObj.readDirectory(fixturePath, {
+        filter: file => file.isDirectory || file.ext === '.txt',
+        fileTransform: file => require('fs').readFileSync(file.fullpath, 'utf8'),
+        dirTransform: (file, value) => Object.getOwnPropertyNames(value).length > 0 ? value : undefined
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('fileTransform - ozen object', () => {
+      const result = dirObj.readDirectory(path.join(fixturePath, 'dir1'), {
+        fileTransform: file => Object.freeze(require(file.fullpath)),
+        dirTransform: (file, value) => Object.getOwnPropertyNames(value).length > 0 ? value : undefined
+      });
+
+      assert.ok(Object.isFrozen(result.fixtures1));
+      assert.throws(() => { result.fixtures1 = {}; });
+      assert.throws(() => { result.fixtures1.test = 'test'; });
+    }),
+
+    test('fileTransform - ignore a file by returning undefined', () => {
+      const expected = {
+        subdir1: {
+          subsubdir1: {},
+          subsubdir2: {}
+        }
+      };
+
+      const result = dirObj.readDirectory(path.join(fixturePath, 'dir.2'), {
+        fileTransform: file => undefined
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('dirTransform - change directory key', () => {
+      const expected = {
+        _dir: { fixtures3: { name: 'dir2/fixtures3.sjon' } },
+        __dir: { ' - bad % file  name ': { name: ' - bad % file \\ name .json' } },
+        dir__: {
+          subdir_: {
+            subsubdir_: {
+              subfixture1: { name: 'subsubdir1/subfixture1.json' },
+              subfixture2: true
+            },
+            subsubdir2: { index: { name: 'subsubdir2/index.js' } }
+          }
+        },
+        dir_: {
+          fixtures1: { name: 'fixture1.json' },
+          fixtures2: { name: 'fixtures2.js' }
+        },
+        root: 'This is the root'
+      };
+
+      const result = dirObj.readDirectory(fixturePath, {
+        dirTransform: (file, value) => {
+          if (file.name !== 'subsubdir2') {
+            file.key = file.key.replace(/[\d.]/g, '_');
+          }
+          return value;
+        }
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('dirTransform - ignore a directory by returning undefined', () => {
+      const expected = { root: 'This is the root' };
+
+      const result = dirObj.readDirectory(fixturePath, {
+        dirTransform: file => undefined
+      });
+
+      assert.deepEqual(result, expected);
+    }),
+
+    test('filter, fileTransform, and dirTransform', () => {
+      const expected = {
+        '3dir': { fixtures3: { name: 'dir2/fixtures3.sjon' } },
+        '4.dir': { '_-_bad_%_file__name_': { name: ' - bad % file \\ name .json' } },
+        'dir.2': {
+          subdir1: {
+            subsubdir1: {
+              subfixture1: { name: 'subsubdir1/subfixture1.json' },
+              subfixture2: true
+            }
+          }
+        },
+        dir1: {
+          fixtures1: { name: 'fixture1.json' },
+          fixtures2: { name: 'fixtures2.js' }
+        },
+        root: 'This is the root'
+      };
+
+      const result = dirObj.readDirectory(fixturePath, {
+        filter: file => (file.isDirectory || file.isRequirable) && file.name !== 'index.js',
+        fileTransform: file => {
+          file.key = file.basename.replace(/\s/g, '_');
+          return require(file.fullpath);
+        },
+        dirTransform: (file, value) => Object.getOwnPropertyNames(value).length > 0 ? value : undefined
+      });
+
+      assert.deepEqual(result, expected);
+    })
+  ];
+
+  const results = tests.reduce((accum, t) => {
+    accum[t ? 'passed' : 'failed'] += 1;
+    return accum;
+  }, {
+    passed: 0,
+    failed: 0
+  });
+
+  console.log(require('util').inspect(results, { colors: true }));
+  assert.equal(results.failed, 0);
+});
